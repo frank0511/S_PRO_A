fb2137861abf873aa722d5933dcbec6dcf8d7a93
AbelTarazona <abeljosetr15@gmail.com>
2016-10-20 15:15:58 -0500

    COMENTARIO :v

--- application/controllers/clientes.php
+++ application/controllers/clientes.php
@@ -11,7 +11,7 @@ class Clientes extends CI_Controller {
         $this->load->model('m_clientes');
         $this->load->helper('Utils');
     }
-    
+    //HOLA MUNDO :v
 	public function index()
 	{
 	    $idUsuario     = $this->session->userdata('idUsuario');
